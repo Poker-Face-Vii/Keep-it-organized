7246c9e525ef8b00db7626ac13990af8fd1d3d66
sajjad Alizadeh <samycrip10@gmail.com>
2019-12-19 15:04:20 +0330

    Add bottom sheet

--- lib/screen/inbox.dart
+++ lib/screen/inbox.dart
@@ -2,25 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:keep_it_organized/store/taskManage/task_manage.dart';
 
-
 class InboxPage extends StatelessWidget {
-  final TaskManage task = TaskManage();
+  final TaskManage _task = TaskManage();
+  B_module modal = new B_module();
 
 
   @override
   Widget build(BuildContext context) {
     return Observer(
         builder: (_) => Scaffold(
-              appBar: new AppBar(title: Text('KIO'),),
+              appBar: new AppBar(
+                title: Text('KIO'),
+              ),
               body: ListView.builder(
-                itemCount: task.count,
+                itemCount: _task.count,
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     title: Text('ff'),
                   );
                 },
               ),
-              floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed: task.increment,),
+              floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: () => modal.mainBoottomSheet(context),
+              ),
             ));
+  }
+}
+
+class B_module {
+  mainBoottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(height: 300.0,padding: EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[Row(
+                children: <Widget>[
+                  Container( child: TextField(autofocus: true,cursorColor: Colors.black,)),
+                  Container(child: Icon(Icons.send),)
+                ],
+              )],
+            ),
+          );
+        });
   }
 }
