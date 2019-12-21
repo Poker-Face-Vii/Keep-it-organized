f528037a271bc41a029c35d4f44026aeeca7ed40
sajjad Alizadeh <samycrip10@gmail.com>
2019-12-22 00:07:45 +0330

    Test page

--- lib/screen/Addpage.dart
+++ lib/screen/Addpage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hive/hive.dart';
 import 'package:keep_it_organized/Components/_Drawer.dart';
+import 'package:keep_it_organized/database/myTask_db.dart';
 import 'package:keep_it_organized/store/TestStore/test_st.dart';
 
 class AddPage extends StatefulWidget {
@@ -11,53 +12,76 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
-  final String _dbname = 'Todo_task';
+  final String _dbname = 'oowwoo';
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Hive.openBox('Todo_task'),
+      future: Hive.openBox<Mytask>('oowwoo'),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           if (snapshot.hasError) {
             return Text(snapshot.error.toString());
           } else {
-        // my local varibales
-        // '******************************************
-        final _dbt = Hive.box(_dbname);
-        final _formKey = GlobalKey<FormState>();
-        final TestSt _state = TestSt();
-        // '******************************************
-        // End varibles part
+            // my local varibales
+            // '******************************************
+            final _dbt = Hive.box<Mytask>(_dbname);
+            final _formKey = GlobalKey<FormState>();
+            final TestSt _state = TestSt();
+            // '******************************************
+            // End varibles part
             return Observer(
-      builder: (_) {
-        return Scaffold(
-            drawer: my_menu(context, 2),
-            appBar: AppBar(
-              title: Text('Add new task'),
-            ),
-            body: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextField( controller: _state.tCo,
-                  ),
-                  SizedBox(height: 25.0,),
-                  IconButton(
-                    icon: Icon(Icons.check),
-                    onPressed: () {
-                      // _state.debug(context);
-                      
-                      _state.addTasktoDB();
-                      _dbt.add(_state.title);
-                      debugPrint('the lenght : ${_dbt.length}');
-                    },
-                  )
-                ],
-              ),
-            ));
-      },
-    );
+              builder: (_) {
+                return Scaffold(
+                    drawer: my_menu(context, 2),
+                    appBar: AppBar(
+                      title: Text('Add new task'),
+                    ),
+                    body: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            controller: _state.tCo,
+                          ),
+                          SizedBox(
+                            height: 25.0,
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.check),
+                            onPressed: () {
+                              // _state.debug(context);
+
+                              _state.addTasktoDB();
+                              _dbt.add(Mytask(_state.title));
+                              debugPrint('the lenght : ${_dbt.length}');
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.star),
+                            onPressed: () {
+                              // _state.debug(context);
+                              debugPrint('the lenght is ${_dbt.length}');
+                              debugPrint('1: ${_dbt.getAt(4).title}');
+                              // debugPrint('2: ${_dbt.getAt(1).title}');
+                              // debugPrint('3: ${_dbt.getAt(2).title}');
+                              // debugPrint('4: ${_dbt.getAt(3).title}');
+                              // debugPrint('5: ${_dbt.getAt(4).title}');
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              _dbt.deleteAt(0);
+                              // _state.debug(context);
+                              debugPrint('the lenght is ${_dbt.length}');
+                            },
+                          ),
+                        ],
+                      ),
+                    ));
+              },
+            );
           }
         } else {
           return Scaffold(
@@ -73,11 +97,4 @@ class _AddPageState extends State<AddPage> {
       },
     );
   }
-
-  @override
-  void dispose() {
-    Hive.box('Todo_task').close();
-    super.dispose();
-  }
 }
-
--- lib/screen/TestPage.dart
+++ lib/screen/TestPage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:keep_it_organized/Components/_Drawer.dart';
+import 'package:keep_it_organized/database/myTask_db.dart';
 import 'package:keep_it_organized/store/TestStore/test_st.dart';
-
-
 
 class TestPage extends StatefulWidget {
   // DB name
@@ -13,37 +13,34 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
-  final String _dbname = 'Todo_task';
+  final String _dbname = 'oowwoo';
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Hive.openBox('Todo_task'),
+      future: Hive.openBox<Mytask>('oowwoo'),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           if (snapshot.hasError) {
             return Text(snapshot.error.toString());
           } else {
-        // my local varibales
-        // '******************************************
-        final _dbt = Hive.box(_dbname);
-        final _formKey = GlobalKey<FormState>();
-        final TestSt _state = TestSt();
-        // '******************************************
-        // End varibles part
+            // my local varibales
+            // '******************************************
+            final _dbt = Hive.box<Mytask>(_dbname);
+            final TestSt _state = TestSt();
+            // '******************************************
+            // End varibles part
             return Observer(
-      builder: (_) {
-        return Scaffold(
-            drawer: my_menu(context, 1),
-            appBar: AppBar(
-              title: Text('Add new task'),
-            ),
-            body: ListView.builder(itemCount: _dbt.length,
-            itemBuilder: (BuildContext context,int index){
-              return;
-            },));
-      },
-    );
+              builder: (_) {
+                return Scaffold(
+                  drawer: my_menu(context, 1),
+                  appBar: AppBar(
+                    title: Text('Add new task'),
+                  ),
+                  body: _buildListView()
+                );
+              },
+            );
           }
         } else {
           return Scaffold(
@@ -60,9 +57,25 @@ class _TestPageState extends State<TestPage> {
     );
   }
 
-  @override
-  void dispose() {
-    Hive.box('Todo_task').close();
-    super.dispose();
-  }
+  
 }
+
+
+Widget _buildListView() {
+return WatchBoxBuilder(
+  box: Hive.box<Mytask>('oowwoo'),
+  builder: (context, contactsBox) {
+    return ListView.builder(
+      itemCount: contactsBox.length,
+      itemBuilder: (BuildContext context, int index) {
+        final contact = contactsBox.getAt(index) as Mytask;
+
+        return ListTile(
+          title: Text(contact.title),
+          trailing: IconButton(icon: Icon(Icons.delete),onPressed: ()=> contactsBox.deleteAt(index),),
+        
+        );
+      },
+    );
+  },
+);}
