0406f468f3dd21bc5bb2650f4c7b0685d61a3f56
sajjad Alizadeh <samycrip10@gmail.com>
2019-12-21 12:03:18 +0330

    test state

---
+++ lib/store/TestStore/test_st.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:mobx/mobx.dart';
+
+// Include generated file
+part 'test_st.g.dart';
+
+// This is the class used by rest of your codebase
+class TestSt = _TestSt with _$TestSt;
+
+// The store-class
+abstract class _TestSt with Store {
+  
+  @observable
+  
+  
+
+  @action
+  
+
+  @action
+  
+  
+}
